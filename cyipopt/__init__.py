4e3f13df562ef4ae21a57edc9429cad48b859bb0
Jason K. Moore <moorepants@gmail.com>
2026-04-05 10:12:07 +0200

    Remove the ipopt_wrapper.py module as it serves no purpose since we purged importing from ipopt.

--- cyipopt/__init__.py
+++ cyipopt/__init__.py
@@ -4,13 +4,12 @@ cyipopt: Python wrapper for the Ipopt optimization package, written in Cython.
 
 Copyright (C) 2012-2015 Amit Aides
 Copyright (C) 2015-2017 Matthias Kümmerer
-Copyright (C) 2017-2025 cyipopt developers
+Copyright (C) 2017-2026 cyipopt developers
 
 License: EPL 2.0
 """
 
 from ipopt_wrapper import *
-from .ipopt_wrapper import *
 from .scipy_interface import *
 from .version import __version__
 from .exceptions import *
